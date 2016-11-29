e306fc949b3dec445b1ccdd8b42455b12fb625a7
ccharris <ccharris27@gmail.com>
2016-11-29 13:29:17 -0500

    shopping list item bean

---
+++ src/main/java/shoppinglistjava1/java/beans/PriorityTexts.java
@@ -0,0 +1,15 @@
+package shoppinglistjava1.java.beans;
+
+public enum PriorityTexts {
+	PRIORITYONE(1, "Grab it now!"), PRIORITYTWO(2, "Need it soon!"), PRIORITYTHREE(3, "It can wait!");
+	
+    private final String text; // in money
+    private final int num;
+    PriorityTexts(int num, String text) {
+    	this.num = num;
+        this.text = text;
+    }
+    public String getText() { return text; }
+	public int getNum() { return num; }
+	
+}
